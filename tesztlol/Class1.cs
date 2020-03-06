b91a7ca43897f8671ea9e47bf822957646369ab8
palflaszlo <pallaszlo15@gmail.com>
2020-03-06 10:18:38 +0100

    inito finito

--- tesztlol/Class1.cs
+++ tesztlol/Class1.cs
@@ -1,12 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace tesztlol
 {
-    public class Class1
+    public class Eloadas
     {
+        private bool[,] bk;
+
+        public Eloadas(int numOfRows, int numOfPlaces)
+        {
+            if (numOfRows <= 0 || numOfPlaces <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.bk = new bool[numOfRows, numOfPlaces];
+            for (int i = 0; i < bk.GetLength(0); i++)
+            {
+                for (int j = 0; j < bk.GetLength(1); j++)
+                {
+                    bk[i, j] = false;
+                }
+            }
+        }
+        public bool book()
+        {
+            for (int i = 0; i < bk.GetLength(0); i++)
+            {
+                for (int j = 0; j < bk.GetLength(1); j++)
+                {
+                    if (bk[i, j] == false)
+                    {
+                        bk[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public int freeSpace()
+        {
+            int freeSpaceIndex = 0;
+            for (int i = 0; i < bk.GetLength(0); i++)
+            {
+                for (int j = 0; j < bk.GetLength(1); j++)
+                {
+                    if (bk[i, j] == false)
+                    {
+                        freeSpaceIndex++;
+                    }
+                }
+            }
+            return freeSpaceIndex;
+        }
+        public bool Full()
+        {
+            if (freeSpace() <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Booked(int numOfRow, int numOfPlace)
+        {
+            if (numOfRow > bk.GetLength(0) || numOfPlace > bk.GetLength(1))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                if (bk[numOfRow, numOfPlace] == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
---
+++ tesztlol/No.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tesztlol;
+using NUnit.Framework;
+
+namespace tesztlol
+{
+    [TestFixture]
+    class No
+    {
+        [TestCase]
+        public void NewPresConsPos()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+        }
+        [TestCase]
+        public void NewPresConsNeg()
+        {
+            Assert.Throws<ArgumentException>(() => { Eloadas eloadas = new Eloadas(-420, -69); });
+        }
+        [TestCase]
+        public void NewPresConsNull()
+        {
+            Assert.Throws<ArgumentException>(() => { Eloadas eloadas = new Eloadas(0, 0); });
+        }
+        [TestCase]
+        public void NewBookPos()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            eloadas.book();
+        }
+        [TestCase]
+        public void BookOnePlaceNotFull()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            eloadas.book();
+            Assert.AreEqual(false, eloadas.Full());
+        }
+        [TestCase]
+        public void BookAllPlaceFull()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            for (int i = 0; i < 28980; i++)
+            {
+                eloadas.book();
+            }
+            Assert.AreEqual(true, eloadas.Full());
+        }
+        [TestCase]
+        public void CheckIfBooked()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            for (int i = 0; i < 28980; i++)
+            {
+                eloadas.book();
+            }
+            Assert.AreEqual(true, eloadas.Booked(6, 9));
+        }
+        [TestCase]
+        public void CheckIfBookedNeg()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            for (int i = 0; i < 350; i++)
+            {
+                eloadas.book();
+            }
+            Assert.AreEqual(false, eloadas.Booked(19, 19));
+        }
+        [TestCase]
+        public void NoFreeSpace()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            for (int i = 0; i < 28980; i++)
+            {
+                eloadas.book();
+            }
+            Assert.AreEqual(0, eloadas.freeSpace());
+        }
+        [TestCase]
+        public void FreeSpacePos()
+        {
+            Eloadas eloadas = new Eloadas(420, 69);
+            for (int i = 0; i < 350; i++)
+            {
+                eloadas.book();
+            }
+            Assert.Greater(eloadas.freeSpace(), 0);
+        }
+    }
+}
